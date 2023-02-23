8d28e9a2785a6aa5da9c5f1aa4e6ade865ef22b4
shirab1993 <64367805+shirab1993@users.noreply.github.com>
2023-02-23 09:08:56 +0200

    commit script that run project & Add .gitignore file

---
+++ Consul-API/vagrant_up.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Use Vagrantfile and run Vagrant commends that Build & Run the VM 
+# Vagrantfile install docker
+vagrant up
+vagrant ssh
+
+echo "#######################################################"
+echo " vagrant up"
+echo "#######################################################"
+
